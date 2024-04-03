ddca9501ceb6959018fa085be310d41c21df181d
Feiyue DU <dydy961008@163.com>
2024-04-03 18:18:18 +0200

    accountinfo

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,19 @@
 class ListsController < ApplicationController
+  before_action :set_account
+
+  def index
+    @places = @account.places
+  end
+
+  def destroy
+    place = @account.places.find(params[:id])
+    place.destroy
+    redirect_to account_places_path(@account), notice: 'Favorite place removed successfully.'
+  end
+
+  private
+
+  def set_account
+    @account = current_user.account
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/explore', to: 'pages#explore'
+  get '/accountinformation', to: 'account#accountinformation'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -17,5 +18,11 @@ Rails.application.routes.draw do
 
   # route for the filter options page
   get '/filter_options', to: 'filter_options#index', as: 'filter_options'
+
+  root 'accounts#show'
+  resources :users, only: [:show, :edit, :update] do
+    resources :pets, except: [:index, :show]
+    resources :place, only: [:index, :destroy]
+  end
 
 end
