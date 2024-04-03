50821314caa3534282bfa9995cc1f0724183d9f6
abhranil das <abhranil.das@ieseg.fr>
2024-04-03 15:40:35 +0200

    trialy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   # def index
   def home
+    # @owner_name = current_user.name if current_user # Assuming you have a 'name' attribute in your User model
   end
 end
