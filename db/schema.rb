60e01e28737c87825dbfeb62d016520a1a2c70fc
Victoria Vallejo <victoria.vallejosastrias@ieseg.fr>
2024-04-03 09:49:02 +0200

    schema fixed

--- db/schema.rb
+++ db/schema.rb
@@ -1,113 +1,93 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Filter Options</title>
-  <style>
-    /* Add left margin for better spacing */
-    body {
-      margin-left: 20px;
-    }
-    .checkbox-label {
-      margin-left: 10px;
-      display: inline-block;
-      width: calc(100% - 25px); /* Adjust width to accommodate checkbox */
-    }
-    .column {
-      float: left;
-      width: 33.33%; /* Set width for each column */
-      padding: 5px; /* Add padding for spacing */
-    }
-    /* Clear floats after each column */
-    .row:after {
-      content: "";
-      display: table;
-      clear: both;
-    }
-    /* Indent property types and their options */
-    .property-type-options {
-      margin-left: 10px;
-    }
-    /* Reduce width of property size options */
-    .pet-size-options .checkbox-label {
-      width: calc(90% - 25px); /* Adjust width to accommodate checkbox */
-    }
-    /* Add margin between rows */
-    .row {
-      margin-bottom: 10px;
-    }
-    /* Adjust margins for special characteristics options */
-    .special-characteristics-options {
-      margin-left: 10px;
-    }
-  </style>
-  <!-- Include Font Awesome CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-</head>
-<body>
-  <h1>Filter Options</h1>
-  <label for="property_type">Property Type:</label><br>
-  <form action="/search" method="GET">
-    <div class="row">
-      <div class="column">
-        <!-- Generate checkboxes based on property types from Place model -->
-        <% Place::PROPERTY_TYPES.each_with_index do |property_type, index| %>
-          <% if index % 3 == 0 %>
-            <input type="checkbox" id="<%= property_type.downcase %>" name="property_type[]" value="<%= property_type %>">
-            <label for="<%= property_type.downcase %>" class="checkbox-label"><%= property_type %></label><br>
-          <% end %>
-        <% end %>
-      </div>
-      <div class="column">
-        <!-- Second column -->
-        <% Place::PROPERTY_TYPES.each_with_index do |property_type, index| %>
-          <% if index % 3 == 1 %>
-            <input type="checkbox" id="<%= property_type.downcase %>" name="property_type[]" value="<%= property_type %>">
-            <label for="<%= property_type.downcase %>" class="checkbox-label"><%= property_type %></label><br>
-          <% end %>
-        <% end %>
-      </div>
-      <div class="column">
-        <!-- Third column -->
-        <% Place::PROPERTY_TYPES.each_with_index do |property_type, index| %>
-          <% if index % 3 == 2 %>
-            <input type="checkbox" id="<%= property_type.downcase %>" name="property_type[]" value="<%= property_type %>">
-            <label for="<%= property_type.downcase %>" class="checkbox-label"><%= property_type %></label><br>
-          <% end %>
-        <% end %>
-      </div>
-    </div>
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-    <!-- Pet Size options -->
-    <div class="row pet-size-options">
-      <div class="column">
-        <label for="pet_size">Pet Size:</label><br>
-        <% Pet::PET_SIZES.each do |pet_size| %>
-          <input type="checkbox" id="<%= pet_size.downcase %>" name="pet_size[]" value="<%= pet_size %>">
-          <label for="<%= pet_size.downcase %>" class="checkbox-label"><%= pet_size %></label><br>
-        <% end %>
-      </div>
-    </div>
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_134325) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-    <!-- Special characteristics options-->
-    <div class="row special-characteristics-options">
-      <div class="column">
-        <label for="special_characteristics">Special Characteristics:</label><br>
-        <input type="checkbox" id="offleash" name="offleash" value="true">
-        <label for="offleash" class="checkbox-label">Offleash</label><br>
-        <input type="checkbox" id="indoor" name="indoor" value="true">
-        <label for="indoor" class="checkbox-label">Indoor</label><br>
-        <input type="checkbox" id="outdoor" name="outdoor" value="true">
-        <label for="outdoor" class="checkbox-label">Outdoor</label><br>
-        <input type="checkbox" id="disposal_station" name="disposal_station" value="true">
-        <label for="disposal_station" class="checkbox-label">Disposal Station</label><br>
-        <input type="checkbox" id="water_access" name="water_access" value="true">
-        <label for="water_access" class="checkbox-label">Water Access</label><br>
-      </div>
-    </div>
+  create_table "lists", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "place_id"
+    t.index ["place_id"], name: "index_lists_on_place_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
-    <br>
+  create_table "pets", force: :cascade do |t|
+    t.string "pet_name"
+    t.date "birthdate"
+    t.string "microchip_number"
+    t.string "breed"
+    t.string "size"
+    t.string "color"
+    t.string "sterilized"
+    t.string "vaccine_card"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
 
-    <input type="submit" value="Apply Filters">
-  </form>
-</body>
-</html>
+  create_table "places", force: :cascade do |t|
+    t.string "place_name"
+    t.string "place_description"
+    t.string "place_type"
+    t.string "address"
+    t.string "city"
+    t.string "country"
+    t.string "zip_code"
+    t.float "longitude"
+    t.float "latitude"
+    t.boolean "tag_offleash"
+    t.boolean "tag_wateraccess"
+    t.boolean "tag_outdoor"
+    t.boolean "tag_indoor"
+    t.boolean "tag_smallpet"
+    t.boolean "tag_mediumpet"
+    t.boolean "tag_largepet"
+    t.boolean "tag_disposalstation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "place_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "description"
+    t.integer "rating"
+    t.index ["place_id"], name: "index_reviews_on_place_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "lists", "users"
+  add_foreign_key "pets", "users"
+  add_foreign_key "reviews", "places"
+  add_foreign_key "reviews", "users"
+end
