1352354fcf27b4bf361e8b380b25c46ad674c695
Charmaine Nicole Ramos <charmainenicole.ramos@ieseg.fr>
2024-04-09 16:57:49 +0200

    add pet modal with save and update list function. no photos yet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,15 +8,22 @@ class PetsController < ApplicationController
     @pet.user = current_user
 
     if @pet.save
-      redirect_to root_path
+      render json: { message: 'Pet added successfully' }, status: :created
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def index
+    @pets = current_user.pets # Assuming pets are associated with the current user
+    respond_to do |format|
+      format.html { render partial: 'pets_list', locals: { pets: @pets } } # Respond with HTML (using a partial)
+      format.json { render json: @pets } # Respond with JSON (optional, if needed)
+    end
+  end
   private
 
   def pet_params
-    params.require(:pet).permit(:pet_name, :birthdate, :microchip_number, :breed, :size, :color, :sterilized, :vaccine_card, :pet_photo)
+    params.require(:pet).permit(:pet_name, :birthdate, :microchip_number, :breed, :size, :color, :sterilized) #:vaccine_card, :pet_photo)
   end
 end
