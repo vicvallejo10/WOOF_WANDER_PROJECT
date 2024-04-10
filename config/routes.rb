76079f0f11639861e97d16928953b2cc6b6eef55
Charmaine Nicole Ramos <charmainenicole.ramos@ieseg.fr>
2024-04-10 10:59:36 +0200

    pushing to align with team

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :pets, only: [:index, :show, :new, :create]  # Example route for pets
+  resources :pets, only: [:index, :show, :new, :create, :destroy]  # Example route for pets
 
   # Defines the root path route ("/")
   # root "posts#index"
