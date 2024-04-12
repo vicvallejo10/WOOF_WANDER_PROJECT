24c11eb15174cb2233d1e97cce600b341c200870
Charmaine Nicole Ramos <charmainenicole.ramos@ieseg.fr>
2024-04-12 15:48:16 +0200

    random 3 places

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,8 @@
 # app/helpers/application_helper.rb
 
 module ApplicationHelper
-  def random_recommendation_of_the_day
-    # Fetch all places from the database
-    places = Place.all
-    # Get a random place from the list of places
-    random_place = places.sample
-    # Return the random place
-    random_place
+  def random_recommendations(number_of_recommendations)
+    recommendations = Place.all.sample(number_of_recommendations)
+    recommendations.shuffle
   end
 end
