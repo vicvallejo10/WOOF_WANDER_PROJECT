ab81779bc971009cbe9dab29cc54d18a1c6df5c4
Charmaine Nicole Ramos <charmainenicole.ramos@ieseg.fr>
2024-04-12 15:06:07 +0200

    recommended places and back button revised

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# app/helpers/application_helper.rb
+
 module ApplicationHelper
+  def random_recommendation_of_the_day
+    # Fetch all places from the database
+    places = Place.all
+    # Get a random place from the list of places
+    random_place = places.sample
+    # Return the random place
+    random_place
+  end
 end
